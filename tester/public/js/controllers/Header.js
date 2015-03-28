7cb1f3f238f621cff6a6df26ca916a6c0829043b
jmaupin82 <jmaupin82@gmail.com>
2015-03-28 17:54:09 -0500

    pre run commit

--- tester/public/js/controllers/Header.js
+++ tester/public/js/controllers/Header.js
@@ -3,21 +3,22 @@
 
 	var Header = function($scope, $http, $modal) {
 		$scope.noUser = true;
-		dpd.users.me(function(user) {
-		    if (user) {
-		        $scope.noUser = false;
-		        window.user = user;
-		        $scope.username = user.username;
-		        $scope.$apply();
-		        console.log(user);
-	       }
-	    });
+		$scope.isContractor = true;
 		$scope.signIn = function() {
 			$modal.open({
 				templateUrl: 'templates/login.html',
 				controller: 'SignIn'
 			});
 		};
+		dpd.users.me(function(user, error){
+			$scope.user = user;
+			$scope.noUser = false;
+			$scope.username = user.username;
+			// dpd.projects.get({contractorUserid: user.id}, function(results, error){
+			// 	console.log("results", results);
+			// 	console.log("error message", error);
+			// });
+		});
 
     $scope.signUp = function() {
       $modal.open({
--- tester/public/js/controllers/Projects.js
+++ tester/public/js/controllers/Projects.js
@@ -1,16 +1,51 @@
 (function () {
   var punchlist = angular.module('punchList');
 
-  var Projects = function($scope, $http, $modal) {
+  var Projects = function($rootscope, $scope, $http, $modal) {
   	//get all projects for the signed in user
-  	if(user){
+  	if($scope.user){
   		console.log("there is a user", user);
-  		//dpd.projects.get({})
+  		//get the list of projects
   	}
 
+	$scope.addProject = function() {
+	  $modal.open({
+	        templateUrl: 'templates/addProject.html',
+	        controller: 'RegisterUser'
+	      });
+	  }
+
+	  $scope.doAddProject = function() {
+	  	debugger;
+	  	if($scope.isClient && $scope.addproject.pid) {
+	  		//only if the user is a client and the pid was entered
+	  		//lookup the project with the given pid
+
+
+	  	}
+	  	else if ($scope.isContractor && $scope.user) {
+	  		//create a new project based on the form informations
+	  		var proj = {
+		  		name: $scope.addproject.name,
+		  		pid: math.random(200), //TODO make this non coliding
+		  		description: $scope.addproject.description,
+		  		contractorUserid : $scope.user.id
+		  	};
+		  	dpd.projects.post(proj, function(success, error){
+		  		if(success){
+		  			console.log("success!", success);
+		  		}
+		  		else {
+		  			console.log("error :(", error);
+		  		}
+		  	});
+	  	}
+	  }
   	
   };
 
   punchlist.controller('Projects', Projects);
+
+  
 
 }());
