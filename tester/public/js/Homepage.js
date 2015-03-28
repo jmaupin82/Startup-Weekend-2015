6e81d08f53aa08086dbef3b39301aee4b8d0428d
V0rtechs <mhunterm21@gmail.com>
2015-03-28 12:36:37 -0500

    login and put the user on window

--- tester/public/js/Homepage.js
+++ tester/public/js/Homepage.js
@@ -1,8 +1,27 @@
 (function () {
   var punchlist = angular.module('punchList');
 
-  var Homepage = function($scope) {
+  var Homepage = function($scope, $http) {
     $scope.oneAtATime = true;
+
+    $scope.user = {};
+
+    $scope.doLogin = function(){
+
+      $http.post('/users/login',{
+        username: $scope.user.name,
+        password: $scope.user.password
+      }).success(function(result){
+        dpd.users.me(function(result, error) {
+          if(error) console.log(error);
+          this.user = result; // put user on the window
+          console.log(this.user);
+          //$scope.user = result;
+        });
+      }).error(function(result){
+        // error
+      })
+    };
 
     $scope.groups = [
       {
